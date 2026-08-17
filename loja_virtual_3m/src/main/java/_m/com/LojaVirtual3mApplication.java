5a3edbe5aef06082582548480fa05f71f4a64d86
Paulo Cezar Henrique Junior <paulopchjr@gmail.com>
2026-08-17 15:16:30 -0300

    commit iniicial do projeto Loja 3 m

---
+++ loja_virtual_3m/src/main/java/_m/com/LojaVirtual3mApplication.java
@@ -0,0 +1,13 @@
+package _m.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtual3mApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtual3mApplication.class, args);
+	}
+
+}
---
+++ loja_virtual_3m/src/test/java/_m/com/LojaVirtual3mApplicationTests.java
@@ -0,0 +1,13 @@
+package _m.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtual3mApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
